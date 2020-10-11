7ff61f05bb87e60c31702b61e5c1e58fbec0ca73
Ashish Kumar <ashish.0422@gmail.com>
2020-10-11 00:03:08 -0500

    adding total number of votes by each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -11,13 +11,29 @@ with open(election_data, 'r') as csvfile:
     csv_header =next(csvreader)
     #Initializing variables
     counter = 0 
+    #Initialize candidates list
+    candidates = []
+    comparison_list = []
    
     for row in csvreader:
         #print(row) 
         counter = counter + 1
+       
+        if row[2] in candidates:
+            comparison_list[candidates.index(row[2])]+=1
+        else:
+            candidates.append(row[2])
+            comparison_list.append(1)
+
     print("Total votes: " + str(counter) + ".")
+    print(comparison_list)
+    print(f"List of candidates: {candidates}")
+    print(f"Votes won by each candidate: {comparison_list}")
 
      #Export Data into a text file
     results=open("Analysis/results.txt","w")
     results.write("Eelction Results \n------------------\n")
     results.write(f"Total Votes: {counter}\n")
+    results.write(f"List of candidates: {candidates}")
+    results.write(f"Votes won by each candidate: {comparison_list}")
+    
